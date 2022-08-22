68e9b9ffb08660c2ccf46c9a74b89a8753715033
sasimi1327 <wonderland1327@gmail.com>
2022-08-23 03:14:30 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,47 @@
+var programSwiper = new Swiper(".programSwiper", {
+  autoplay: {
+    // delay: 2000,
+    // disableOnInteraction: false,
+    disableOnInteraction: false,
+    delay: 0,
+  },
+  speed: 1500,
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 16,
+  breakpoints: {
+    767: {
+      slidesPerView: 5,
+    },
+    991: {
+      slidesPerView: 7,
+    },
+  },
+});
+
+var powerswiper = new Swiper(".powerswiper", {
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var programSwiper = new Swiper(".courseSwiper", {
+  slidesPerView: 2,
+  spaceBetween: 8,
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+    },
+    991: {
+      slidesPerView: 6,
+    },
+  },
+});
